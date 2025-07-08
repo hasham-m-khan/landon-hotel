201e9ba61636f48e208e641977e2c714ecfcaa08
Hasham M Khan <hasham.m.khan@outlook.com>
2025-07-08 01:44:03 -0500

    Adds front page room reservation form.

--- src/main/webapp/frontend/src/app/app.ts
+++ src/main/webapp/frontend/src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'frontend';
+export class App implements OnInit {
+
+    public submitted: boolean = false;
+    roomsearch!: FormGroup;
+
+    ngOnInit(): void {
+        this.roomsearch = new FormGroup({
+            checkin: new FormControl(''),
+            checkout: new FormControl(''),
+        })
+    }
+
+    onSubmit({value, valid}: {value: RoomSearch, valid: boolean}) {
+        console.log(value);
+    }
+
+}
+
+export interface RoomSearch {
+    checkin: string;
+    checkout: string;
 }
